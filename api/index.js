18f3fedbdb3554fc8689e0d446df88163784025e
loupdemon <azzougaghiles@hotmail.com>
2023-01-18 13:53:11 +0100

    instal express mongoose dotenv multer nodemon

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// const router = express.Router();
+// const Post = require('../models/Post');
+// const User = require('../models/User');
+// const Category = require('../models/Category');
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+
+
+const app = express();
+//app.use(express.json());
+
+
+console.log("hello world");
+app.listen("5000", () => {
+    console.log("Backend is running!");
+    });
