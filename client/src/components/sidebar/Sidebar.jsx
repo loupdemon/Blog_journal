f64d5be866875b9736f77a0c1911a5177f71c300
loupdemon <azzougaghiles@hotmail.com>
2023-01-18 11:49:56 +0100

    links and design final

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 import "./sidebar.css";
 
 export default function Sidebar() {
@@ -19,22 +19,34 @@ export default function Sidebar() {
         <span className="sidebarTitle">CATEGORIES</span>
         <ul className="sidebarList">
           <li className="sidebarListItem">
-           
+            <Link className="link" to="/posts?cat=Life">
+              Life
+            </Link>
           </li>
           <li className="sidebarListItem">
-            
+            <Link className="link" to="/posts?cat=Music">
+              Music
+            </Link>
           </li>
           <li className="sidebarListItem">
-            
+            <Link className="link" to="/posts?cat=Sport">
+              Sport
+            </Link>
           </li>
           <li className="sidebarListItem">
-            
+            <Link className="link" to="/posts?cat=Style">
+              Style
+            </Link>
           </li>
           <li className="sidebarListItem">
-            
+            <Link className="link" to="/posts?cat=Tech">
+              Tech
+            </Link>
           </li>
           <li className="sidebarListItem">
-           
+            <Link className="link" to="/posts?cat=Cinema">
+              Cinema
+            </Link>
           </li>
         </ul>
       </div>
