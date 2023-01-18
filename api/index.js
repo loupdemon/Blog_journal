1d63ef5203b40da0040544e6993c06065da8a297
loupdemon <azzougaghiles@hotmail.com>
2023-01-18 14:11:01 +0100

    configuration mongoose et dotenv

--- api/index.js
+++ api/index.js
@@ -5,13 +5,35 @@ const express = require('express');
 // const Category = require('../models/Category');
 // const bcrypt = require('bcrypt');
 // const jwt = require('jsonwebtoken');
-
+const mongoose = require("mongoose");
 
 const app = express();
-//app.use(express.json());
+//DOTENV environnement de travai
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+//usage de mongoose pour gestion de sql
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    //useFindAndModify: false,
+}).then(() => {
+    console.log("DB connection successful!");
+}).catch((err) => {
+    console.log(err);
+});
+
+
+// app.use("/lama",(req,res)=>{
+
+//     console.log("hey  this main url");
+//     //res.send("Hello World");
+// });
 
 
 console.log("hello journal");
 app.listen("5000", () => {
     console.log("Backend is running!");
-    });
+});
