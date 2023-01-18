0bbed8fe0d8418b0d8dce773afde3e8b9906d741
loupdemon <azzougaghiles@hotmail.com>
2023-01-18 16:09:55 +0100

    resolve probleme connexion post with roote and add bcrypt in order to hash pd

--- api/index.js
+++ api/index.js
@@ -1,39 +1,31 @@
 const express = require('express');
-// const router = express.Router();
-// const Post = require('../models/Post');
-// const User = require('../models/User');
-// const Category = require('../models/Category');
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
 const mongoose = require("mongoose");
-
 const app = express();
-//DOTENV environnement de travai
+const authRoute = require('./routes/auth');
 const dotenv = require("dotenv");
-
+// mongoose.connect(process.env.MONGO_URL);
+mongoose.set("strictQuery", false);
+//DOTENV environnement de travai
 dotenv.config();
 
+app.use(express.json());
 //usage de mongoose pour gestion de sql
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    //useFindAndModify: false,
-}).then(() => {
-    console.log("DB connection successful!");
-}).catch((err) => {
-    console.log(err);
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+        //useFindAndModify: false,
+    }).then(() => {
+        console.log("DB connection successful!");
+    }).catch((err) => {
+        console.log(err);
 });
 
-
-// app.use("/lama",(req,res)=>{
-
-//     console.log("hey  this main url");
-//     //res.send("Hello World");
-// });
+app.use("/api/auth", authRoute);
 
 
-console.log("hello journal");
-app.listen("5000", () => {
+// console.log("hello journal");
+app.listen("6000", () => {
     console.log("Backend is running!");
 });
