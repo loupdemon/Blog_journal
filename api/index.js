f8d5e4d475a30ca64b433595c6a043fc4c2554c8
loupdemon <azzougaghiles@hotmail.com>
2023-01-19 16:05:24 +0100

    all request api'

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const app = express();
+
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/categories');
+
 const dotenv = require("dotenv");
+
+const multer = require("multer");
 
 //MONGOOSE CONFIG
 mongoose.set("strictQuery", false);
@@ -27,6 +31,22 @@ mongoose
     }).catch((err) => {
         console.log(err);
 });
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+        // cb(null, "hello.jpeg");
+    },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
+
 
 //ROUTES MIDDLEWARE
 app.use("/api/auth", authRoute);
